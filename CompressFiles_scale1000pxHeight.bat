4012e221d559c660f72f06bc1cde03cdfa9e44a9
Yannick Günzel <yannickguenzel@yahoo.de>
2021-09-09 19:58:12 +0200

    Initial upload
    > CompressFiles
    Compresses .mp4 files without changing resolution
    
    > CompressFiles_scale1000pxHeight
    Compresses .mp4 files and rescales the videos to have a height of 1000px
    
    > CompressFiles_scale1000pxWidth
    Compresses .mp4 files and rescales the videos to have a width of 1000px

---
+++ CompressFiles_scale1000pxHeight.bat
@@ -0,0 +1,31 @@
+:: Turn off the command echoing feature
+@echo off
+
+:: Delayed Expansion will cause variables within a batch file to be expanded at execution time.
+:: This allows to create and assign variables during a for-loop
+setlocal enabledelayedexpansion
+
+:: Create a folder for the compressed videos
+mkdir compressed
+
+:: Iterate over all files in the current folder that end with .mp4
+for %%a IN ("*.mp4") do (
+
+	:: Get the current file name
+	set FileName_In=%%a
+
+	:: Cut of the file extension ".mp4", i.e. the last for characters of the string
+	set FileName_In=!FileName_In:~0,-4!
+
+	:: Concatenate original file name with "_compressed.mp4" to create the the output file's name
+	set FileName_Out=!FileName_In!.mp4
+
+	:: Run ffmpeg
+	ffmpeg -i "%%a" -vcodec libx264 -crf 20 -r 25 -vf scale=-1:1000 "compressed\!FileName_OUT!" 
+
+)
+pause
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # batCompressFiles
 Batch file compressing of all .mp4 files in the current folder using ffmpeg
+Compressed files will be saved in a new subfolder /compressed with the same filename.
+
+
+## ffmpeg settings:
+(can be changed via right click > edit > line 24)
+-vcodec libx264		-	Video codec          (Lossless H.264)
+-crf 20			-	constant rate factor (The range of the CRF scale is 0–51, where 0 is lossless, 23 is the default, and 51 is worst quality possible.)
+-r 25			-	frame rate           (fps)
